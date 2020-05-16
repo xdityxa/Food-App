c5021fbc83d6b3ac0d83f3d4f75db0d981553a83
Aditya Gupta <aditya.gupta181099@gmail.com>
2020-05-16 15:05:55 +0530

    delete recipe functionality

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +21,27 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Your Filter'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Filter!'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                SwitchListTile(
+                  title: Text('Gluten'),
+                  value: null,
+                  onChanged: null,
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
